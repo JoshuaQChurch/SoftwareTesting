4c0857bea950d787fbdb2eaa7d60217d89f0521c
JoshuaQChurch <JoshuaQChurch@gmail.com>
2016-05-02 18:11:24 -0500

    Initial Commit

---
+++ QA4_Main.py
@@ -0,0 +1,28 @@
+import math
+import unittest
+import sys
+from types import *
+
+while True:
+    print('1. Calculate BMI')
+    print('2. Calculate Distance Formula')
+    print('3. Calculate Retirement')
+    print('4. Verify Emails')
+    print('Enter anything else to exit.\n')
+
+    choice = (input('Please choose an option: '))
+
+    if choice == '1':
+        #main_bmi()
+        continue
+    elif choice == '2':
+        #calculate_distance()
+        continue
+    elif choice == '3':
+        #retirement_plan()
+        continue
+    elif choice == '4':
+        #email_verifier()
+        continue
+    else:
+        sys.exit()
