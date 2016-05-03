958b7e4d0d7ed747bf1d31c895fce478df931276
SkylineHorizon <skylinehorizon@hotmail.com>
2016-05-02 21:55:44 -0500

    Double Implemented

--- Retirement_Implemented_Code.py
+++ Retirement_Implemented_Code.py
@@ -4,3 +4,6 @@ import math
 def take_percentage(value, percent):
     return value * percent
 
+
+def double(value):
+    return value * 2
--- Retirement_test.py
+++ Retirement_test.py
@@ -10,10 +10,26 @@ class TakePercentage(unittest.TestCase):
 
     def test_percentage_1(self):
         self.assertEqual(take_percentage(100, 0.25), 25)
+
     def test_percentage_2(self):
         self.assertEqual(take_percentage(-50, 0.10), -5)
+
+    def test_percentage_3(self):
+        self.assertEqual(take_percentage(115, 0), 0)
+
+
+# Implemented by Jeffry Herzog
+class Double(unittest.TestCase):
+
+    def test_double_1(self):
+        self.assertEqual(double(2), 4)
+
+    def test_double_2(self):
+        self.assertEqual(double(-4.5), -9)
+
+    def test_double_3(self):
+        self.assertEqual(double(0), 0)
 
 
 if __name__ == '__main__':
     unittest.main(exit=False)
-
