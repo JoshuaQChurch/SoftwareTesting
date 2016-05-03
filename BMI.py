e35dbc210cde3a2978d20505e6a0fc641620b00e
cah835 <corey@Coreys-MacBook-Pro.local>
2016-05-02 21:40:59 -0500

    BMI test update
    BMI.py updated to include feet to inches test, screen shots, all pass
    and fails logged and updated.

--- BMI.py
+++ BMI.py
@@ -5,3 +5,6 @@ import math
 
 def square(value):
     return value * value
+
+def ft_to_in(value):
+    return value * 12
--- BMI_test.py
+++ BMI_test.py
@@ -4,6 +4,7 @@ import sys
 from BMI import *
 from types import *
 
+#done by Corey Henry
 
 class Squaretesting(unittest.TestCase):
 
@@ -18,6 +19,19 @@ class Squaretesting(unittest.TestCase):
 
     def test_square4(self):
         self.assertEqual(square(1), 1)
+
+#done by Corey Henry
+
+class Feet_to_inches_testing(unittest.TestCase):
+    def test_fti1(self):
+        self.assertEqual(ft_to_in(1), 12)
+
+    def test_fti2(self):
+        self.assertEqual(ft_to_in(2), 24)
+
+    def test_fti3(self):
+        self.assertEqual(ft_to_in(4), 48)
+    
 
 if __name__ == '__main__':
     unittest.main(exit=False)
