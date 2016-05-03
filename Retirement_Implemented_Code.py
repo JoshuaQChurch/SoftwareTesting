472d120507f2044dfffd47e88e57372f891fa281
SkylineHorizon <skylinehorizon@hotmail.com>
2016-05-02 22:21:16 -0500

    Finished Retirement

--- Retirement_Implemented_Code.py
+++ Retirement_Implemented_Code.py
@@ -1,9 +1,24 @@
-import math
-
-
 def take_percentage(value, percent):
     return value * percent
 
 
 def double(value):
     return value * 2
+
+
+def aging(value):
+    return value + 1
+
+
+def retirement(age, annual_salary, percentage_saved, goal):
+
+    total_savings = 0
+
+    while total_savings < goal:
+        total_savings += double(take_percentage(annual_salary, percentage_saved))
+        age = aging(age)
+
+        if age >= 100:
+            break
+
+    return age
