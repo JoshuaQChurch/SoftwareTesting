8c2a22997e4c45c0941112eb8f4b109cb4b14802
Leress <jdw751@msstate.edu>
2016-05-04 18:53:13 -0500

    added  to def retirement/tests
    ...

--- Unit_Tests_Retirement.py
+++ Unit_Tests_Retirement.py
@@ -11,11 +11,20 @@ class TakePercentage(unittest.TestCase):
     def test_percentage_1(self):
         self.assertEqual(take_percentage(100, 0.25), 25)
 
-    def test_percentage_2(self):
-        self.assertEqual(take_percentage(-50, 0.10), -5)
+##    def test_percentage_2(self):
+##        self.assertEqual(take_percentage(-50, 0.10), -5)
 
     def test_percentage_3(self):
         self.assertEqual(take_percentage(115, 0), 0)
+
+    #Jon Williams
+    def test_negative_input(self):
+        self.assertRaises(OutOfRangeError,take_percentage, -1, .1)
+        
+    def test_negative_input(self):
+        self.assertRaises(OutOfRangeError,take_percentage, 1, -.1)
+        
+    
 
 
 # Implemented by Jeffry Herzog
@@ -24,11 +33,24 @@ class Double(unittest.TestCase):
     def test_double_1(self):
         self.assertEqual(double(2), 4)
 
-    def test_double_2(self):
-        self.assertEqual(double(-4.5), -9)
+##    def test_double_2(self):
+##        self.assertEqual(double(-4.5), -9)
 
     def test_double_3(self):
         self.assertEqual(double(0), 0)
+
+    #Jon Williams
+    def test_negative_input(self):
+        self.assertRaises(OutOfRangeError,double, -1)
+
+    def test_negative_input(self):
+        self.assertRaises(OutOfRangeError,aging, -1)
+
+    def test_negative_input_for_goal(self):
+        self.assertRaises(OutOfRangeError,retirement, 1, 100, .1, -1)
+
+    def test_negative_input_for_age(self):
+        self.assertRaises(OutOfRangeError,retirement, -1, 100, .1, -1)
 
 
 if __name__ == '__main__':
--- def_Retirement.py
+++ def_Retirement.py
@@ -1,18 +1,33 @@
+class OutOfRangeError(ValueError):
+    pass
+
+
 def take_percentage(value, percent):
+    if (value < 0 or percent < 0):
+        raise OutOfRangeError('cannot enter neagive number')
+    
     return value * percent
 
 
 def double(value):
+    if (value < 0 ):
+        raise OutOfRangeError('cannot enter neagive number')
+    
     return value * 2
 
 
 def aging(value):
+    if (value < 0 ):
+        raise OutOfRangeError('cannot enter neagive number')
     return value + 1
 
 
 def retirement(age, annual_salary, percentage_saved, goal):
 
     total_savings = 0
+
+    if (goal < 0 ):
+        raise OutOfRangeError('cannot enter neagive number')
 
     while total_savings < goal:
         total_savings += double(take_percentage(annual_salary, percentage_saved))
