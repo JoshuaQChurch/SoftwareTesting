31ed61d7bc831d5a328940f203adea3c393653bf
JoshuaQChurch <JoshuaQChurch@gmail.com>
2016-05-04 17:28:27 -0500

    Additions to tests

--- Unit_Tests_EmailVerifier.py
+++ Unit_Tests_EmailVerifier.py
@@ -5,14 +5,29 @@ from def_EmailVerifier import *
 # Written by Joshua Church
 class Domain(unittest.TestCase):
     def test_domain_1(self):
-        self.assertEqual("edu", verifyDomain("jqc10@dasi.msstate.edu"))
+        self.assertNotEqual("edu", verifyDomain("jqc10@dasi.msstate.edu"))
 
-    def test_dot_symbol(self):
+    def test_dot_symbol_1(self):
         self.assertFalse(verifyDotSymbol("jqc10@dasi..msstate.edu"))
+
+    def test_dot_symbol_2(self):
+        self.assertTrue(verifyDotSymbol("jqc10@dasi.msstate.edu"))
 
     def test_at_symbol_1(self):
         self.assertTrue(verifyAtSymbol("jjh258@msstate.edu"))
 
-    def test_at_symbol_2
+    def test_at_symbol_2(self):
+        self.assertFalse(verifyAtSymbol("jjh258msstate.edu"))
+
+    def test_verify_content_1(self):
+        self.assertTrue(verifyContent("randomEmail2252234@joshua.co"))
+
+    def test_verify_content_2(self):
+        self.assertFalse(verifyContent("thisshouldFail.@.com"))
+
+    def test_verify_content_3(self):
+        self.assertFalse(verifyContent("thisShouldFail@@test.com"))
+
+    
 if __name__ == '__main__':
     unittest.main(exit=False)
