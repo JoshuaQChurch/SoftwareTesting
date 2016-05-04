183594dca2540214dfd5accb422b80b9b0cd5504
SkylineHorizon <skylinehorizon@hotmail.com>
2016-05-04 16:47:25 -0500

    QA4_Main Update

--- QA4_Main.py
+++ QA4_Main.py
@@ -2,19 +2,37 @@ import math
 import unittest
 import sys
 from types import *
-
+from def_BMI import *
+from def_Retirement import *
+from def_DistanceFormula import *
+from def_EmailVerifier import *
 
 
 def try_except(string):
     while True:
         try:
             value = float(input(string))
+            if value <= 0:
+                print("ERROR: Please enter a value greater than 0!")
+                continue
             break
         except ValueError:
-            print("Please insert a valid numeric value!")
+            print("ERROR: Please enter a valid numeric value!")
 
     return value
 
+def retirement_plan():
+    age = try_except("\nPlease enter your current age: ")
+    annual_salary = try_except("Please enter your current salary: ")
+    percentage_saved = try_except("Please enter the percentage saved (as a decimal): ")
+    goal = try_except("Please enter your desired goal: ")
+
+    goal_age = retirement(age, annual_salary, percentage_saved, goal)
+
+    if goal_age >= 100:
+        print("Sorry, goal not met!")
+    else:
+        print("Your goal will be met at the age of:", int(goal_age), "\n")
 
 
 
@@ -35,7 +53,7 @@ def main():
             #calculate_distance()
             continue
         elif choice == '3':
-            #retirement_plan()
+            retirement_plan()
             continue
         elif choice == '4':
             #email_verifier()
